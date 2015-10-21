6184f6817b33738bc83c9de10cf7654696d8d88e
“ylno” <micha@frankl.info>
2015-10-21 09:57:12 +0200

    Fix Bug in battery handling which leaded to a wrong display of the hour

--- src/greenclock-face.c
+++ src/greenclock-face.c
@@ -9,18 +9,19 @@
 
 #define TICK_LENGTH  10
 
-#define ANIMATION_DURATION 500
+#define ANIMATION_DURATION 1000
 #define ANIMATION_DELAY    600
 
 // XY startpoints where battery is drawn
 #define BAT_START_X  112
 #define BAT_START_Y  2
 
-//#define DEBUG false
+//#define DEBUG true
 
 typedef struct {
   int hours;
   int minutes;
+  tm tick_time;
 } Time;
 
 static Window *s_main_window;
@@ -31,7 +32,6 @@ static Time s_last_time, s_anim_time;
 static int s_radius = 0, s_color_channels[3];
 static bool s_animating = true;
 
-
 static BatteryChargeState batteryState;
 static Layer *mydrawings_layer;
 
@@ -39,10 +39,16 @@ static Layer *mydrawings_layer;
 /*************************** AnimationImplementation **************************/
 
 static void animation_started(Animation *anim, void *context) {
+  #ifdef DEBUG
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "animation_started");
+  #endif
   s_animating = true;
 }
 
 static void animation_stopped(Animation *anim, bool stopped, void *context) {
+  #ifdef DEBUG
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "animation_stopped");
+  #endif
   s_animating = false;
 
   // repaint after animation to show the other stuff
@@ -52,6 +58,9 @@ static void animation_stopped(Animation *anim, bool stopped, void *context) {
 }
 
 static void animate(int duration, int delay, AnimationImplementation *implementation, bool handlers) {
+  #ifdef DEBUG
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "animate");
+  #endif
   Animation *anim = animation_create();
   animation_set_duration(anim, duration);
   animation_set_delay(anim, delay);
@@ -68,15 +77,25 @@ static void animate(int duration, int delay, AnimationImplementation *implementa
 
 /************************************ UI **************************************/
 
-static void tick_handler(struct tm *tick_time, TimeUnits changed) {
+static void tick_handler(struct tm *tick_time_new, TimeUnits changed) {
   // Store time
-  s_last_time.hours = tick_time->tm_hour;
-  s_last_time.hours -= (s_last_time.hours > 12) ? 12 : 0;
-  s_last_time.minutes = tick_time->tm_min;
+  #ifdef DEBUG
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "tick_handler");
+  #endif
 
-  for(int i = 0; i < 3; i++) {
-    s_color_channels[i] = rand() % 256;
-  }
+  s_last_time.tick_time = *tick_time_new;
+  s_last_time.hours = tick_time_new->tm_hour;
+  s_last_time.hours -= (s_last_time.hours > 12) ? 12 : 0;
+  s_last_time.minutes = tick_time_new->tm_min;
+
+  #ifdef DEBUG
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "tick_hanlder hours %i minutes %i", s_last_time.hours, s_last_time.minutes);
+  #endif
+
+
+//  for(int i = 0; i < 3; i++) {
+//    s_color_channels[i] = rand() % 256;
+//  }
 
   // Redraw
   if(s_canvas_layer) {
@@ -95,28 +114,36 @@ static void update_proc(Layer *layer, GContext *ctx) {
   #endif
   if(COLORS) {
     //graphics_context_set_fill_color(ctx, GColorFromRGB(s_color_channels[0], s_color_channels[1], s_color_channels[2]));
-    graphics_context_set_fill_color(ctx, GColorBlack);
+    graphics_context_set_fill_color(ctx, GColorWhite);
     graphics_fill_rect(ctx, GRect(0, 0, 144, 168), 0, GCornerNone);
   }
 
-  time_t temp = time(NULL); 
-  struct tm *tick_time = localtime(&temp);
+  //time_t temp = time(NULL); 
+  //time_t temp = 1445347103; 
+  
+  //struct tm *tick_time = localtime(&temp);
 
   graphics_context_set_stroke_color(ctx, GColorBlack);
   graphics_context_set_stroke_width(ctx, 2);
 
   graphics_context_set_antialiased(ctx, ANTIALIASING);
 
+  // Draw a shadow
+  graphics_context_set_fill_color(ctx, GColorLightGray);
+  GPoint shadow_center = GPoint(s_center.x + 4, s_center.y + 4);
+  graphics_fill_circle(ctx, shadow_center, s_radius);
+
   // White clockface
   graphics_context_set_fill_color(ctx, GColorWhite);
   graphics_fill_circle(ctx, s_center, s_radius);
 
   // Draw outline
-  graphics_context_set_stroke_color(ctx, GColorYellow);
+  graphics_context_set_stroke_color(ctx, GColorBlack);
   graphics_draw_circle(ctx, s_center, s_radius);
 
   // Don't use current time while animating
   Time mode_time = (s_animating) ? s_anim_time : s_last_time;
+  struct tm *tick_time = &mode_time.tick_time;
 
   if(!s_animating) {
     // Draw day ring
@@ -169,15 +196,28 @@ static void update_proc(Layer *layer, GContext *ctx) {
   graphics_context_set_stroke_width(ctx, 1);
   
   // Adjust for minutes through the hour
+  #ifdef DEBUG
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "TRIG_MAX_ANGLE %i animating %s hours %i", TRIG_MAX_ANGLE, s_animating ? "true" : "false", mode_time.hours);
+  #endif
   float minute_angle = TRIG_MAX_ANGLE * mode_time.minutes / 60;
-  float hour_angle;
+  float hour_angle = 0.0;
   if(s_animating) {
     // Hours out of 60 for smoothness
     hour_angle = TRIG_MAX_ANGLE * mode_time.hours / 60;
   } else {
     hour_angle = TRIG_MAX_ANGLE * mode_time.hours / 12;
   }
+  #ifdef DEBUG
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "minute angle %i", (int)minute_angle);
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "hour angle %i", (int)hour_angle);
+  #endif
   hour_angle += (minute_angle / TRIG_MAX_ANGLE) * (TRIG_MAX_ANGLE / 12);
+
+  #ifdef DEBUG
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "hour angle %i", (int)hour_angle);
+APP_LOG(APP_LOG_LEVEL_DEBUG, "hour angle1 %i", (int)2.1113);
+  #endif
+  
 
   // Plot hands
   GPoint minute_hand = (GPoint) {
@@ -195,18 +235,22 @@ static void update_proc(Layer *layer, GContext *ctx) {
     graphics_context_set_stroke_width(ctx, 4);
     graphics_context_set_stroke_color(ctx, GColorBlack);
     graphics_draw_line(ctx, s_center, hour_hand);
-    graphics_context_set_stroke_width(ctx, 2);
-    graphics_context_set_stroke_color(ctx, GColorDarkGreen);
-    graphics_draw_line(ctx, s_center, hour_hand);
+    
+    // Fill Hand with inner white
+    //graphics_context_set_stroke_width(ctx, 1);
+    //graphics_context_set_stroke_color(ctx, GColorWhite);
+    //graphics_draw_line(ctx, s_center, hour_hand);
   } 
 
   if(s_radius > HAND_MARGIN) {
     graphics_context_set_stroke_width(ctx, 3);
     graphics_context_set_stroke_color(ctx, GColorBlack);
     graphics_draw_line(ctx, s_center, minute_hand);
-    graphics_context_set_stroke_width(ctx, 1);
-    graphics_context_set_stroke_color(ctx, GColorDarkGreen);
-    graphics_draw_line(ctx, s_center, minute_hand);
+
+    // Fill Hand with inner white
+    //graphics_context_set_stroke_width(ctx, 1);
+    //graphics_context_set_stroke_color(ctx, GColorWhite);
+    //graphics_draw_line(ctx, s_center, minute_hand);
   }
 
   graphics_context_set_stroke_width(ctx, 1);
@@ -215,7 +259,7 @@ static void update_proc(Layer *layer, GContext *ctx) {
     static char buffer[] = "00:00";
     
     //graphics_context_set_text_color(ctx, GColorIslamicGreen);
-    graphics_context_set_text_color(ctx, GColorWhite);
+    graphics_context_set_text_color(ctx, GColorBlack);
     strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
     // FONT_KEY_FONT_FALLBACK FONT_KEY_BITHAM_30_BLACK     GColorSpringBud
     graphics_draw_text(ctx, buffer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD), GRect(102, 140,  89, 25), GTextOverflowModeWordWrap, GTextAlignmentLeft, NULL);
@@ -225,7 +269,12 @@ static void update_proc(Layer *layer, GContext *ctx) {
     graphics_draw_text(ctx, buffer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD), GRect(2, -11,  80, 24), GTextOverflowModeWordWrap, GTextAlignmentLeft, NULL);
   }
 
-  //APP_LOG(APP_LOG_LEVEL_DEBUG, "finished");
+  #ifdef DEBUG
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "hours %i minutes %i", mode_time.hours, mode_time.minutes);
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "finished");
+  #endif
+  
+  
 }
 
 static void my_layer_update_proc(Layer *my_layer, GContext* ctx) {
@@ -236,7 +285,7 @@ static void my_layer_update_proc(Layer *my_layer, GContext* ctx) {
     //---draw 2 rectangles to represent the battery---
 
     //graphics_context_set_stroke_color(ctx, GColorMalachite);
-    graphics_context_set_stroke_color(ctx, GColorDarkGreen);
+    graphics_context_set_stroke_color(ctx, GColorBlack);
     graphics_context_set_fill_color(ctx, GColorMintGreen);
 
     GRect rect1;
@@ -266,6 +315,9 @@ static void my_layer_update_proc(Layer *my_layer, GContext* ctx) {
 
 
 static void window_load(Window *window) {
+  #ifdef DEBUG
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "window load");
+  #endif
   Layer *window_layer = window_get_root_layer(window);
   GRect window_bounds = layer_get_bounds(window_layer);
 
@@ -283,6 +335,9 @@ static void window_load(Window *window) {
 }
 
 static void window_unload(Window *window) {
+  #ifdef DEBUG
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "window unload");
+  #endif
   layer_destroy(s_canvas_layer);
   layer_destroy(mydrawings_layer);
   battery_state_service_unsubscribe();
@@ -295,12 +350,18 @@ static int anim_percentage(uint32_t dist_normalized, int max) {
 }
 
 static void radius_update(Animation *anim, AnimationProgress dist_normalized) {
+  #ifdef DEBUG
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "radius update");
+  #endif
   s_radius = anim_percentage(dist_normalized, FINAL_RADIUS);
 
   layer_mark_dirty(s_canvas_layer);
 }
 
 static void hands_update(Animation *anim, AnimationProgress dist_normalized) {
+  #ifdef DEBUG
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "hands_update");
+  #endif
   s_anim_time.hours = anim_percentage(dist_normalized, hours_to_minutes(s_last_time.hours));
   s_anim_time.minutes = anim_percentage(dist_normalized, s_last_time.minutes);
 
@@ -308,17 +369,28 @@ static void hands_update(Animation *anim, AnimationProgress dist_normalized) {
 }
 
 static void battery_state_handler(BatteryChargeState charge) {
+  #ifdef DEBUG
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "battery_state_handler");
+  #endif
   //---mark the drawing layer as dirty so as to force
   // a redraw---
-  batteryState = charge;
-  layer_mark_dirty(mydrawings_layer);
+  
+  if(batteryState.charge_percent != charge.charge_percent) {
+    batteryState = charge;
+    layer_mark_dirty(mydrawings_layer);
+  }
+  
 }
 
 static void init() {
+  #ifdef DEBUG
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "init");
+  #endif
   srand(time(NULL));
 
   time_t t = time(NULL);
   struct tm *time_now = localtime(&t);
+
   tick_handler(time_now, MINUTE_UNIT);
 
   s_main_window = window_create();
@@ -359,6 +431,9 @@ static void deinit() {
 
 int main() {
   init();
+  #ifdef DEBUG
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "main");
+  #endif
   app_event_loop();
   deinit();
 }
